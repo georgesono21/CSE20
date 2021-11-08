963f787fcb41f13297a27e3ab26a950abbb6e9a2
georgesono21 <93453893+georgesono21@users.noreply.github.com>
2021-11-07 21:21:37 -0800

    Update Assignment 6.1.py

--- Assignment 6.1.py
+++ Assignment 6.1.py
@@ -7,115 +7,123 @@
 #Acknowledgements: Harikishna Kuttivelli, PythonDocs
 
 #defining main function
+
+def dict_equal(d1,d2):
+  #how it works: we make four lists. this is because each dictionary has key and values. we the compare the keylists together to see if they have the same elements, if not we return false (same with the valuelists)
+  keycheck_d1 = []
+  valcheck_d1 = []
+  keycheck_d2 = []
+  valcheck_d2 = []
+
+  #adding each key and value to an empty list
+  for x in d1.keys():
+    keycheck_d1.append(x)
+    valcheck_d1.append(d1[x])
+  for x in d2.keys():
+    keycheck_d2.append(x)
+    valcheck_d2.append(d2[x])
+
+  #if theres more keys in one keylist than the other, than return False
+  if len(keycheck_d1) != len(keycheck_d2):
+    return False
+  else:
+    #if the dict1 key is not in dict2keylist return false (same with value)
+    for x in range(len(keycheck_d1)):
+      if keycheck_d1[x] not in keycheck_d2:
+        return False
+      if valcheck_d1[x] not in valcheck_d2:
+        return False
+    #returns True if the for loop is finished, this means that the dicts are same
+    return True
+
+def count_words(sentence):
+  #how it works: to process the sentence, we get rid of any punctuation and lower  case all the letters in the sentence. we then string split it at the blank spaces to form it into a list (so its iterable). then we use a for loop to check how many times a word is in the sentencelist.
+  sentencedict = {}
+  #getting rid of any punctuation
+  sentence = sentence.split(",")
+  sentence = "".join(sentence)
+  sentence = sentence.split(".")
+  sentence = "".join(sentence)
+  sentence = sentence.split()
+
+  #lower casing all elements in the sentencelist
+  for i in range(len(sentence)):
+    sentence[i] = sentence[i].lower()
+
+  for x in sentence:
+    counter = 0
+    for i in range(len(sentence)):
+      if x == sentence[i]:
+        counter += 1
+      #this for loop keeps checking if the word (x) is in the sentence. as we already know theres one of it (as were using that to compare) the range includes the whole entire list (so x is counted). it checks if its in the sentence by checking it with the index value at i in the sentence list.
+
+    sentencedict[x] = counter
+    #assigning the key value pair in the sentence dictionary
+  return sentencedict
+
+def morse(message):
+  #how it works: we split the message at the spaces so we get an iterable list. we then make a for loop inside of a for loop (forloopception) that iterates each letter in the word and translates it to its morse counterpart.
+  #defining the dictionary for morse code (thank you Hari)
+  code = {
+    'A': '.-',
+    'B': '-...',
+    'C': '-.-.',
+    'D': '-..',
+    'E': '.',
+    'F': '..-.',
+    'G': '--.',
+    'H': '....',
+    'I': '..',
+    'J': '.---',
+    'K': '-.-',
+    'L': '.-..',
+    'M': '--',
+    'N': '-.',
+    'O': '---',
+    'P': '.--.',
+    'Q': '--.-',
+    'R': '.-.',
+    'S': '...',
+    'T': '-',
+    'U': '..-',
+    'V': '...-',
+    'W': '.--',
+    'X': '-..-',
+    'Y': '-.--',
+    'Z': '--..',
+    }
+  #splitting the mssage
+  message = message.split()
+
+  #what the morsemessage is gonna be (we just defining it rn tho)
+  actualmessage = ""
+
+  #for loop ception: the first for loop is so all the words in the message list are iterated
+  
+  for x in range(len(message)):
+    wordinmorse = ""
+    #forloop pt2: so we iterate each letter in the word from messagelist
+    counter = 0
+    for i in message[x]:
+      counter += 1
+      if message[x] == message[-1] and counter == len(message[x]):
+        wordinmorse += code[i.upper()]
+      else:
+        wordinmorse += code[i.upper()] + " "
+      #as the morsecode thing only accepts uppercase letters, we translate that into an upper character so it has a value because its a key
+      
+
+      
+    if x == (len(message)-1):
+      actualmessage += wordinmorse
+      #if we are on the last number of the list we dont use the / for a space
+    else:
+      #else we just add the / as theres gonna be another word
+      actualmessage += wordinmorse + "/ "
+  return actualmessage
+
 def main():
   
-  def dict_equal(d1,d2):
-    #how it works: we make four lists. this is because each dictionary has key and values. we the compare the keylists together to see if they have the same elements, if not we return false (same with the valuelists)
-    keycheck_d1 = []
-    valcheck_d1 = []
-    keycheck_d2 = []
-    valcheck_d2 = []
-
-    #adding each key and value to an empty list
-    for x in d1.keys():
-      keycheck_d1.append(x)
-      valcheck_d1.append(d1[x])
-    for x in d2.keys():
-      keycheck_d2.append(x)
-      valcheck_d2.append(d2[x])
-
-    #if theres more keys in one keylist than the other, than return False
-    if len(keycheck_d1) != len(keycheck_d2):
-      return False
-    else:
-      #if the dict1 key is not in dict2keylist return false (same with value)
-      for x in range(len(keycheck_d1)):
-        if keycheck_d1[x] not in keycheck_d2:
-          return False
-        if valcheck_d1[x] not in valcheck_d2:
-          return False
-      #returns True if the for loop is finished, this means that the dicts are same
-      return True
-
-  def count_words(sentence):
-    #how it works: to process the sentence, we get rid of any punctuation and lower case all the letters in the sentence. we then string split it at the blank spaces to form it into a list (so its iterable). then we use a for loop to check how many times a word is in the sentencelist.
-    sentencedict = {}
-    #getting rid of any punctuation
-    sentence = sentence.split(",")
-    sentence = "".join(sentence)
-    sentence = sentence.split(".")
-    sentence = "".join(sentence)
-    sentence = sentence.split()
-
-    #lower casing all elements in the sentencelist
-    for i in range(len(sentence)):
-      sentence[i] = sentence[i].lower()
-
-    
-    for x in sentence:
-      counter = 0
-      for i in range(len(sentence)):
-        if x == sentence[i]:
-          counter += 1
-        #this for loop keeps checking if the word (x) is in the sentence. as we already know theres one of it (as were using that to compare) the range includes the whole entire list (so x is counted). it checks if its in the sentence by checking it with the index value at i in the sentence list.
-
-      sentencedict[x] = counter
-      #assigning the key value pair in the sentence dictionary
-    return sentencedict
-
-  def morse(message):
-    #how it works: we split the message at the spaces so we get an iterable list. we then make a for loop inside of a for loop (forloopception) that iterates each letter in the word and translates it to its morse counterpart.
-    #defining the dictionary for morse code (thank you Hari)
-    code = {
-      'A': '.-',
-      'B': '-...',
-      'C': '-.-.',
-      'D': '-..',
-      'E': '.',
-      'F': '..-.',
-      'G': '--.',
-      'H': '....',
-      'I': '..',
-      'J': '.---',
-      'K': '-.-',
-      'L': '.-..',
-      'M': '--',
-      'N': '-.',
-      'O': '---',
-      'P': '.--.',
-      'Q': '--.-',
-      'R': '.-.',
-      'S': '...',
-      'T': '-',
-      'U': '..-',
-      'V': '...-',
-      'W': '.--',
-      'X': '-..-',
-      'Y': '-.--',
-      'Z': '--..',
-      }
-    #splitting the mssage
-    message = message.split()
-    counter = 0
-    #what the morsemessage is gonna be (we just defining it rn tho)
-    actualmessage = ""
-
-    #for loop ception: the first for loop is so all the words in the message list are iterated
-    for x in range(len(message)):
-      wordinmorse = ""
-      #forloop pt2: so we iterate each letter in the word from messagelist
-      for i in message[x]:
-        wordinmorse += code[i.upper()] + " "
-        #as the morsecode thing only accepts uppercase letters, we translate that into an upper character so it has a value because its a key
-        counter += 1
-      if x == (len(message)-1):
-        actualmessage += wordinmorse
-        #if we are on the last number of the list we dont use the / for a space
-      else:
-        #else we just add the / as theres gonna be another word
-        actualmessage += wordinmorse + " / "
-    return actualmessage
-
   #TEST CASES
 
   print(dict_equal({"name1": "Alice", "name2": "Bob"}, {"name2": "Bob", "name1": "Alice"}))
